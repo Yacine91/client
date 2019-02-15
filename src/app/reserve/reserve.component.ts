abc5599676923e0c50b25c30ff630a295c22e728
Yacine Slimani <slimani.yacine91@outlook.com>
2019-02-15 11:33:51 +0100

    mise a jour

--- src/app/reserve/reserve.component.ts
+++ src/app/reserve/reserve.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms'
+import { formArrayNameProvider } from '@angular/forms/src/directives/reactive_directives/form_group_name';
 
 @Component({
   selector: 'app-reserve',
@@ -9,9 +10,10 @@ import { FormGroup, FormBuilder } from '@angular/forms'
 export class ReserveComponent implements OnInit {
   reservationForm: FormGroup;
   date = new Date();
+  isScheduleOk = false;
   timeOptions = ["12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00"]
   peopleOptions = [1, 2, 3, 4, 5, 6, 7, 8]
-  client = {first_name: "", last_name: "", email: "", phone: ""}
+  client = { first_name: "", last_name: "", email: "", phone: "" }
 
   constructor(private fb: FormBuilder) { }
 
@@ -27,4 +29,22 @@ export class ReserveComponent implements OnInit {
     })
   }
 
+  finalizeReservation(form) {
+    if (form.value) {
+      console.log('valid!')
+      console.log(form.value)
+      console.log(form)
+    }
+  }
+
+  saveReservation(){
+    if(this.reservationForm.valid){
+      console.log(this.reservationForm)
+      this.isScheduleOk = true
+    }
+  }
+
+
+
 }
+
